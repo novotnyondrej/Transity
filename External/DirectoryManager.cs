bcf26d412fac97e11370151c19db5d0de0e76cb1
novotnyondrej <ondrej.novotny1410@gmail.com>
2024-10-12 19:30:56 +0200

    v-0.1.1x - Tridy pro zachazeni se soubory a slozkami

---
+++ External/DirectoryManager.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+
+namespace Transity.External
+{
+	//Stara se o slozky a jejich spravu
+	internal static class DirectoryManager
+	{
+		//Vyhodnoti zda cesta existuje nebo ne
+		public static bool Exists(string path)
+		{
+			return Directory.Exists(path);
+		}
+		//Vytvori slozku
+		public static bool Create(string path)
+		{
+			Directory.CreateDirectory(path);
+			return true;
+		}
+		//Smaze slozku
+		public static bool Delete(string path)
+		{
+			Directory.Delete(path);
+			return true;
+		}
+	}
+}
---
+++ External/FileManager.cs
@@ -0,0 +1,31 @@
+﻿using System.IO;
+
+namespace Transity.External
+{
+	//Stara se o soubory, jejich nacitani a zapis do nich
+	internal static class FileManager
+	{
+		//Zda soubor existuje
+		public static bool FileExists(string fileName)
+		{
+			return File.Exists(fileName);
+		}
+		//Pokusi se precist obsah souboru
+		public static string GetContents(string fileName)
+		{
+			//Kontrola existence souboru
+			if (!FileExists(fileName)) throw new Exception();
+			//Pokus o cteni souboru
+			string contents = File.ReadAllText(fileName);
+			return contents;
+		}
+		//Pokusi se zapsat obsah do souboru
+		public static bool PutContents(string fileName, string contents)
+		{
+			//Zapis do souboru
+			File.WriteAllText(fileName, contents);
+			
+			return true;
+		}
+	}
+}
